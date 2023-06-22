af9959bca2852e9938b4d1ed1a714beb9fe7a7ab
ignam03 <ignafree97@gmail.com>
2023-06-22 12:25:42 -0300

    added validation of date and min maxi  into task and register

--- src/utils/validateForm.ts
+++ src/utils/validateForm.ts
@@ -8,15 +8,45 @@ export const loginValidate = yup.object().shape({
 });
 
 export const registerValidate = yup.object().shape({
-  email: yup.string().trim().required("Email is required"),
-  password: yup.string().trim().required("Password is required"),
-  firstName: yup.string().trim().required("firstName is required"),
-  lastName: yup.string().trim().required("lastName is required"),
+  email: yup
+    .string()
+    .trim()
+    .required("Email is required")
+    .email("Invalid email"),
+  password: yup
+    .string()
+    .trim()
+    .required("Password is required")
+    .min(8, "min 8 characters")
+    .max(20, "max 20 characters"),
+  firstName: yup
+    .string()
+    .trim()
+    .required("firstName is required")
+    .min(3, "min 3 characters")
+    .max(10, "  max 10 characters"),
+  lastName: yup
+    .string()
+    .trim()
+    .required("lastName is required")
+    .min(3, "min 3 characters")
+    .max(10, "  max 10 characters"),
 });
 
 export const TaskValidate = yup.object().shape({
-  name: yup.string().trim().required("Name is required"),
-  description: yup.string().trim().max(50, "max 50 characters"),
-  loadDate: yup.string().trim(),
-  taskType: yup.string().trim(),
+  name: yup
+    .string()
+    .trim()
+    .required("Name is required")
+    .min(10, "min 10 characters"),
+  description: yup
+    .string()
+    .trim()
+    .min(10, "min 10 characters")
+    .max(50, "max 50 characters"),
+  loadDate: yup
+    .date()
+    .min(new Date(), "Min date must be today ")
+    .required("Date is required"),
+  taskType: yup.string().trim().required("Task type is required"),
 });
